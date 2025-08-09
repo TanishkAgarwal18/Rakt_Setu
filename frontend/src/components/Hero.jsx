b2724820e236d4ad7648c9f502bba404d9dcdafd
Tanishk Agarwal <tanishkagarwal2022@vitbhopal.ac.in>
2025-08-09 13:59:24 +0530

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = ()=>{
         <div className="bg-[url('/hero1.jpg')] bg-no-repeat bg-cover bg-center h-[85vh] px-[200px]">  
             
          <div className="flex flex-col text-white w-[50%] pt-[10%]">
-           <span className="text-[30px] mt-3">Donate, save a life, make a difference!</span>
+           <span className="text-[30px] mt-3">Save a life, make a difference!</span>
            <h1 className="text-[38px] mt-3">Let your blood flow for a cause be a donor, be a life-saver.
             </h1>
 
@@ -18,5 +18,6 @@ const Hero = ()=>{
 }
 
 export default Hero ;
+
 
 
